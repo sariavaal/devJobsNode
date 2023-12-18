30886416dcd1fa431c7e32346185efdd50ebfe9a
sariavaal <sariavaal@gmail.com>
2023-12-18 16:26:58 -0300

    views de publicar vacante

---
+++ controllers/vacantesController.js
@@ -0,0 +1,6 @@
+exports.formularioNuevaVacante = (req, res) => {
+    res.render('nueva-vacante', {
+        nombrePagina: 'Nueva vacante',
+        tagline: 'Llena el formulario para publicar tu vacante',
+    })
+}
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const mongoose = require('mongoose');
+require('./config/db')
 const express = require('express');
 const exphbs = require('express-handlebars'); 
 const path = require('path');
 const router = require('./routes');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+require('dotenv').config({ path: 'variables.env' });
 
 const app = express();
 //habilitar template engine
@@ -21,6 +27,21 @@ app.set('view engine', 'handlebars');
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cookieParser());
+
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.DATABASE,
+        mongooseConnection: mongoose.connection,
+        ttl: 24 * 60 * 60
+    })
+}));
+
+
 app.use('/', router());
 
-app.listen(5000);
+app.listen(process.env.PUERTO);
